36b53fa113818180fcffda9ac875fe0e714144ed
SHUN　YAMAZAKI <112524165+1464406@users.noreply.github.com>
2022-09-19 16:06:00 +0900

    Add Task 2022/09/14
    中級演習の3,4を行いました。

---
+++ how-to-code/script.js
@@ -0,0 +1,34 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+// 指数関数的成長 (Exponential growth) を実装してみましょう。変数 の値を変更して、2 のべき乗を求めましょう。
+let value = 1;
+console.log(value);
+
+value += value;
+console.log(value);
+
+value += value;
+console.log(value);
+
+value += value;
+console.log(value);
+
+value += value;
+console.log(value);
+
+value += value;
+console.log(value);
+
+value += value;
+console.log(value);
+
+
+// 文字列結合に挑戦しましょう。いろいろな変数を連結して、期待している結果を表示してください。
+const firstName = "Shun";
+const lastName = "Yamazaki";
+const city = "Nara";
+
+console.log(firstName + " " + lastName);
+
+console.log("Hello, my name is " + firstName + " " + lastName + ". I live in " + city + ".");
